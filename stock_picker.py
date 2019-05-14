e8382ece6101deabea604a4ea6e056e55eed7fe8
Aditya Bansal <adi.bansal241996@gmail.com>
2019-05-14 07:44:02 +0530

    stock_picker.py: Add basic driving function for all helper functions.

--- stock_picker.py
+++ stock_picker.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 from collections import defaultdict, OrderedDict
 from dateutil.parser import parse as dateparser
 import math
@@ -64,3 +64,42 @@ def build_stock_dict(csv_filename: str) -> Dict[str: Dict[datetime.datetime, flo
                 stocks[row[0]][dateparser(row[1])] = row[2]
                 line_count += 1
     return stocks
+
+def take_action(stocks: Dict[str: Dict[datetime.datetime, float]],
+                stock_name: str,
+                start_date: datetime.datetime,
+                end_date: datetime.datetime
+                ) -> Tuple[float, float, datetime.datetime, datetime.datetime, float]:
+    if stock_name not in stocks:
+        raise 'Stock name not in DS error!'
+    contextual_stocks = stocks[stock_name]
+
+    # Stock list generation. We should be fine using this kinda logic up until
+    # something around 10 mil userbase in terms of memory.
+    stock_list = []
+    last_date = start_date
+    for date in contextual_stocks:
+        if date <= start_date:
+            last_date = date
+        else:
+            break
+    last_price = contextual_stocks[last_date]
+    delta = date + datetime.timedelta(days=1)
+    date = start_date
+    dates = set(contextual_stocks.keys())
+    while date <= end_date:
+        if date in dates:
+            stock_list.append(contextual_stocks[date])
+            last_price = stock_list[-1]
+        else:
+            stock_list.append(last_price)
+        date = date + delta
+
+    mean = get_mean_of_stocks(stock_list)
+    sd = get_sd_of_stocks(stock_list)
+    buydelta, selldelta = find_buy_sell_price_index(stock_list)
+    buyd = start_date + datetime.timedelta(days=buydelta)
+    selld = start_date + datetime.timedelta(days=selldelta)
+    profit = find_profit_for_buy_sell(stock_list[buydelta], stock_list[selldelta], 100)
+
+    return (mean, sd, buyd, selld, profit)
